d08956d90c1d980ef15cbce235fa118262c11617
Shaun van Zyl <shaun.michael107@gmail.com>
2020-06-11 11:07:12 +0200

    updates to gulp4/gulp4-run-sequence

--- config.js
+++ config.js
@@ -15,5 +15,100 @@ System.config({
   },
 
   map: {
-     }
+    "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.5.2",
+    "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.3",
+    "aurelia-fetch-client": "npm:aurelia-fetch-client@1.8.2",
+    "aurelia-pal-browser": "npm:aurelia-pal-browser@1.8.1",
+    "aurelia-polyfills": "npm:aurelia-polyfills@0.1.2",
+    "aurelia-router": "npm:aurelia-router@1.7.1",
+    "babel": "npm:babel-core@5.8.38",
+    "babel-runtime": "npm:babel-runtime@5.8.38",
+    "core-js": "npm:core-js@1.2.7",
+    "github:jspm/nodelibs-assert@0.1.0": {
+      "assert": "npm:assert@1.5.0"
+    },
+    "github:jspm/nodelibs-buffer@0.1.1": {
+      "buffer": "npm:buffer@5.6.0"
+    },
+    "github:jspm/nodelibs-path@0.1.0": {
+      "path-browserify": "npm:path-browserify@0.0.0"
+    },
+    "github:jspm/nodelibs-process@0.1.2": {
+      "process": "npm:process@0.11.10"
+    },
+    "github:jspm/nodelibs-util@0.1.0": {
+      "util": "npm:util@0.10.3"
+    },
+    "github:jspm/nodelibs-vm@0.1.0": {
+      "vm-browserify": "npm:vm-browserify@0.0.4"
+    },
+    "npm:assert@1.5.0": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "buffer": "github:jspm/nodelibs-buffer@0.1.1",
+      "object-assign": "npm:object-assign@4.1.1",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "util": "npm:util@0.10.3"
+    },
+    "npm:aurelia-dependency-injection@1.5.2": {
+      "aurelia-metadata": "npm:aurelia-metadata@1.0.6",
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
+    },
+    "npm:aurelia-event-aggregator@1.0.3": {
+      "aurelia-logging": "npm:aurelia-logging@1.5.2"
+    },
+    "npm:aurelia-fetch-client@1.8.2": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
+    },
+    "npm:aurelia-metadata@1.0.6": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
+    },
+    "npm:aurelia-pal-browser@1.8.1": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
+    },
+    "npm:aurelia-polyfills@0.1.2": {
+      "aurelia-pal": "npm:aurelia-pal@1.8.2"
+    },
+    "npm:aurelia-route-recognizer@1.3.2": {
+      "aurelia-path": "npm:aurelia-path@1.1.5"
+    },
+    "npm:aurelia-router@1.7.1": {
+      "aurelia-dependency-injection": "npm:aurelia-dependency-injection@1.5.2",
+      "aurelia-event-aggregator": "npm:aurelia-event-aggregator@1.0.3",
+      "aurelia-history": "npm:aurelia-history@1.2.1",
+      "aurelia-logging": "npm:aurelia-logging@1.5.2",
+      "aurelia-path": "npm:aurelia-path@1.1.5",
+      "aurelia-route-recognizer": "npm:aurelia-route-recognizer@1.3.2"
+    },
+    "npm:babel-runtime@5.8.38": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:buffer@5.6.0": {
+      "base64-js": "npm:base64-js@1.3.1",
+      "ieee754": "npm:ieee754@1.1.13"
+    },
+    "npm:core-js@1.2.7": {
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "path": "github:jspm/nodelibs-path@0.1.0",
+      "process": "github:jspm/nodelibs-process@0.1.2",
+      "systemjs-json": "github:systemjs/plugin-json@0.1.2"
+    },
+    "npm:inherits@2.0.1": {
+      "util": "github:jspm/nodelibs-util@0.1.0"
+    },
+    "npm:path-browserify@0.0.0": {
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:process@0.11.10": {
+      "assert": "github:jspm/nodelibs-assert@0.1.0",
+      "fs": "github:jspm/nodelibs-fs@0.1.2",
+      "vm": "github:jspm/nodelibs-vm@0.1.0"
+    },
+    "npm:util@0.10.3": {
+      "inherits": "npm:inherits@2.0.1",
+      "process": "github:jspm/nodelibs-process@0.1.2"
+    },
+    "npm:vm-browserify@0.0.4": {
+      "indexof": "npm:indexof@0.0.1"
+    }
+  }
 });
